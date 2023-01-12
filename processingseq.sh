f0b7d572aafa094842984476499cf7c7c61c8530
StephanieAFlowers <flowers9@uic.edu>
2023-01-12 14:22:15 -0600

    Update processingseq.sh

--- processingseq.sh
+++ processingseq.sh
@@ -1,33 +1,25 @@
 
 
-samples=("XXX" "XXX")
 
-##Trim samples with skewer
+#Set an array of sample names
+samples=("1" "2")
 
-for sample in "${samples[@]}"
-do
-
-skewer -m pe -q 25 -Q 25 ${sample}_R1.fastq ${sample}_R2.fastq -o ${sample}-trimmed --threads 40
-
-done
-
-#Deduplicate (bbmap) and subsample reads (seqtk)- output files into seperate folders
-
-for sample in "${samples[@]}"
-do
+exec > >(tee -a test4log.txt) 2>&1
 
 # Set the input file paths for paired-end reads
+
+for sample in "${samples[@]}"
+do
   sample1="${sample}-trimmed-pair1.fastq"
   sample2="${sample}-trimmed-pair2.fastq"
-
-
-    base_sample=$(echo ${sample} | grep -o '[0-9]\{2\}$')
-    directory="output/${base_sample}"
-    mkdir -p $directory
+  
+  # create a new directory for each sample
+  directory="output/${sample}"
+  mkdir -p $directory
     
-    dedupe_command="dedupe.sh in=${sample1},${sample2} out=${directory}/${base_sample}_trimmed-dedupe.fastq ac=f mlp=100 mop=100 -Xmx20g"
-    subsample_command="seqtk sample -s 11 ${directory}/${base_sample}_trimmed-dedupe.fastq 50000 > ${directory}/${base_sample}_trimmed-dedupe-50K.fastq"
-    split_command="bbsplitpairs.sh in=${directory}/${base_sample}_trimmed-dedupe-50K.fastq out=${directory}/${base_sample}_trimmed-dedupe-50K-split_R1.fastq out2=${directory}/${base_sample}_trimmed-dedupe-50K-split_R2.fastq"
+    dedupe_command="dedupe.sh in=${sample1},${sample2} out=${directory}/${sample}_trimmed-dedupe.fastq ac=f mlp=100 mop=100 -Xmx20g"
+    subsample_command="seqtk sample -s 11 ${directory}/${sample}_trimmed-dedupe.fastq 50000 > ${directory}/${sample}_trimmed-dedupe-50K.fastq"
+    split_command="bbsplitpairs.sh in=${directory}/${sample}_trimmed-dedupe-50K.fastq out=${directory}/${sample}_trimmed-dedupe-50K-split_R1.fastq out2=${directory}/${sample}_trimmed-dedupe-50K-split_R2.fastq"
 
 eval ${dedupe_command}
 if [ $? -ne 0 ]; then
@@ -46,7 +38,24 @@ if [ $? -ne 0 ]; then
     echo "Error running command: ${split_command}"
     exit 1
 fi
-
 done
 
 
+# RGI
+for sample in "${samples[@]}"
+do
+    rgi_directory="output/${sample}"
+    bwt_command="rgi bwt --threads 8 --read_one ${rgi_directory}/${sample}_trimmed-dedupe-50K-split_R1.fastq --read_two ${rgi_directory}/${sample}_trimmed-dedupe-50K-split_R2.fastq --aligner kma --output ${rgi_directory}/${sample} --debug --clean --local --include_wildcard"
+    kmer_query_command="rgi kmer_query --threads 8 --input ${rgi_directory}/${sample}.sorted.length_100.bam --kmer_size 61 --bwt --output ${rgi_directory}/${sample}.kmer_query.out --debug --local"
+    eval ${bwt_command}
+    if [ $? -ne 0 ]; then
+        echo "Error running command: ${bwt_command}"
+        exit 1
+    fi
+
+    eval ${kmer_query_command}
+    if [ $? -ne 0 ]; then
+        echo "Error running command: ${kmer_query_command}"
+        exit 1
+    fi
+done
